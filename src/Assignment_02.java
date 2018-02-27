5c7a70c63e8da47502f4c0ec67dc41c689cf052a
willany_lp@hotmail.com <willany@willanys-MacBook-Pro.local>
2018-02-27 18:13:02 -0300

    Given a number n, print n asterisks on one line.

---
+++ src/Assignment_02.java
@@ -0,0 +1,12 @@
+public class Assignment_02 {
+    public static void main(String[] args){
+
+        int number = 10;
+        int i = 0;
+
+        while (i < number){
+            System.out.print("*");
+            i = i + 1;
+        }
+    }
+}

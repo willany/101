42caac66d14279931ce573dccca0d41d069f882c
willany_lp@hotmail.com <willany@willanys-MacBook-Pro.local>
2018-02-28 10:49:25 -0300

    Given a number n, print a centered triangle

--- src/Assignment_05.java
+++ src/Assignment_05.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Assignment_05 {
+    public static void main(String[] args){
+
+        int number = 3;
+        String asterisk = "*";
+
+        for (int i = 0; i < number; i++){
+            for (int j = number; j > i; j--){
+                System.out.print(" ");
+            }
+
+            System.out.println(asterisk);
+            asterisk = asterisk + "**";
+        }
+    }
 }

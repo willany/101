0a0c94c51095921f8c1c8824cd4122f44f3efcd1
willany <willany_lp@hotmail.com>
2018-03-01 13:28:51 -0300

    Write a method generate(int n) that given an integer N will return a list of integers such that the numbers are the factors of N and are arranged in increasing numerical order.

--- src/Assignment_05.java
+++ src/Assignment_05.java
@@ -1,7 +1,7 @@
 public class Assignment_05 {
     public static void main(String[] args){
 
-        int number = 3;
+        int number = 5;
         String asterisk = "*";
 
         for (int i = 0; i < number; i++){
